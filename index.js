4c17bf145ce7fcdbdc164f67b50ec7062fa5fd98
sein <seinkane01@gmail.com>
2023-12-16 13:42:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function appendCat(name) {
+  return cats.concat(name); 
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function prependCat(name) {
+  return [name, ...cats]; 
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1); 
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function removeFirstCat() {
+  return cats.slice(1); 
+}
+
